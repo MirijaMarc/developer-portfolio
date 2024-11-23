43bc9fe721072a2d633e1279e82614bf1cadca2f
MirijaMarc <mirijarazafimbelo30@gmail.com>
2024-11-23 21:03:16 +0300

    comment stack

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/users/28434478/mirija-marc',
   leetcode: "https://leetcode.com/said3812/",
   devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1YlZMlQBN4pv4nXJCwKrg2LL8TGt_L9BB/view?usp=drive_link"
 }
