99a523c9c667d60024c50940b3a2a43a874b767f
MirijaMarc <mirijarazafimbelo30@gmail.com>
2024-11-26 11:11:07 +0300

    rectif

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -2,7 +2,7 @@ export const experiences = [
   {
     id: 1,
     title: 'Full-stack developer',
-    company: "Kante Comapny",
+    company: "Kante Company",
     duration: "(Feb 2023 - Nov 2024)"
   },
 ]
