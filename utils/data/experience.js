bd95fea233b261f7212502d38653e9e504f52f97
MirijaMarcNexthope <mirija@nexthope.net>
2025-05-12 12:28:06 +0300

    [fix] change order of experience

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,16 +1,16 @@
 export const experiences = [
   {
     id: 1,
-    title: 'Full-stack developer',
-    company: "Kante Company",
-    duration: "(Jul 2024 - Nov 2024)",
-    url: "/kanteco.jpg"
-  },
-  {
-    id: 2,
     title: 'Backend developer',
     company: "Nexthope",
     duration: "(Feb 2024 - Now)",
     url: "/nexthope.png"
   },
+  {
+    id: 2,
+    title: 'Full-stack developer',
+    company: "Kante Company",
+    duration: "(Jul 2024 - Nov 2024)",
+    url: "/kanteco.jpg"
+  }
 ]
